aa01d869d93c4de7f85be66c092f3979c998329b
Micho Todorovich <micho.todorovich@gmail.com>
2018-10-08 10:36:26 -0500

    refactor

--- Engine/source/InputMessage.cpp
+++ Engine/source/InputMessage.cpp
@@ -8,25 +8,25 @@ using namespace mt;
 
  void InputMessage::_MouseMove(__int32 x, __int32 y)
 {
-	Engine::GetInputHandler()._MouseMove(x, y);
+	Engine::GetInputManager()._MouseMove(x, y);
 }
 
 void InputMessage::_MouseDown(WPARAM btnState)
 {
-	Engine::GetInputHandler()._MouseDown(btnState);
+	Engine::GetInputManager()._MouseDown(btnState);
 }
 
 void InputMessage::_MouseUp(WPARAM btnState)
 {
-	Engine::GetInputHandler()._MouseUp(btnState);
+	Engine::GetInputManager()._MouseUp(btnState);
 }
 
 void InputMessage::_KeyDown(WPARAM vk_key)
 {
-	Engine::GetInputHandler()._KeyDown(vk_key);
+	Engine::GetInputManager()._KeyDown(vk_key);
 }
 
 void InputMessage::_KeyUp(WPARAM vk_key)
 {
-	Engine::GetInputHandler()._KeyUp(vk_key);
+	Engine::GetInputManager()._KeyUp(vk_key);
 }
