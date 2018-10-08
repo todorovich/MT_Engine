6da269b54b1952ec72afa70e199dccc600187f64
Micho Todorovich <micho.todorovich@gmail.com>
2018-10-08 10:32:47 -0500

    added

---
+++ Engine/source/Alarm.cpp
@@ -0,0 +1,33 @@
+#include "Alarm.hpp"
+
+using namespace mt;
+
+void Alarm::Tick(TimePoint current_tick_time, TimePoint previous_tick_time, Duration delta_time)
+{
+	// Not triggered or paused
+	if (_has_triggered == false && _is_paused == false)
+	{
+		if (current_tick_time >= _alarm_time)
+		{
+			_has_triggered = true;
+
+			_callback();
+		}
+	}
+}
+
+void Alarm::Pause(TimePoint time_paused)
+{
+	_time_paused = time_paused;
+	_is_paused = true;
+}
+
+void Alarm::Continue(TimePoint time_continued)
+{
+	// Offset alarm time by the amount of time spent paused;
+	_alarm_time += time_continued - _time_paused;
+
+	_time_paused = TimePoint(0ns);
+
+	_is_paused = false;
+}
---
+++ Engine/source/Timer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "precompiled.hpp"
+
+#include "Alarm.hpp"
+
+namespace mt
+{
+	class Timer : public Alarm
+	{		
+	public:
+
+		Timer(Duration offset, std::function<void()> callback)
+			: Alarm(Clock::now() + offset, callback)
+		{}
+
+		~Timer() = default;
+
+		Timer(const Timer& other) = delete;
+
+		Timer(Timer&& other)
+			: Alarm(std::move(other))
+		{}
+
+		Timer& operator=(const Timer& other) = delete;
+
+		Timer& operator=(Timer&& other)
+		{
+			Alarm::operator=(std::move(other));
+		};
+	};
+}
